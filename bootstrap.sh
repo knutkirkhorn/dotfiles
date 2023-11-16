7b041871a627749985cc91b9bc00eb15ed2f975f
Knut Kirkhorn <knutkirk@hotmail.com>
2023-11-16 11:57:29 +0100

    Create extra file if it doesn't exist

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,9 @@ cp .aliases ~/.aliases
 cp .functions ~/.functions
 cp .bash_profile ~/.bash_profile
 
+# Check if .extra exists and create it if it doesn't
+if [ ! -f ~/.extra ]; then
+	touch ~/.extra
+fi
+
 source ~/.bash_profile
