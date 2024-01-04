604f7ec0ef5b3385a40019dbf6d2f4bb25bb6dd2
Knut Kirkhorn <knutkirk@hotmail.com>
2024-01-04 14:24:22 +0100

    Add global gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,10 @@ git pull
 cp .aliases ~/.aliases
 cp .functions ~/.functions
 cp .bash_profile ~/.bash_profile
+cp .gitignore ~/.gitignore
+
+# Set global gitignore
+git config --global core.excludesfile ~/.gitignore
 
 # Check if .extra exists and create it if it doesn't
 if [ ! -f ~/.extra ]; then
