a3f0f4e8d611dcea94825dd5efd9d16917a17b31
Knut Kirkhorn <knutkirk@hotmail.com>
2025-04-01 11:24:46 +0200

    Add git-credential-manager

--- macos.sh
+++ macos.sh
@@ -4,4 +4,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Homebrew apps
-brew install --cask firefox docker visual-studio-code cursor raycast spotify tableplus postman ghostty proxyman
+brew install --cask firefox docker visual-studio-code cursor raycast spotify tableplus postman ghostty proxyman git-credential-manager
