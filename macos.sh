f9d6859f9b44694a3f3053c75bd027d73a7612ee
Knut Kirkhorn <knutkirk@hotmail.com>
2025-09-08 14:28:06 +0200

    Add more homebrew packages

--- macos.sh
+++ macos.sh
@@ -3,6 +3,11 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Install Homebrew packages
+# jwt-cli (https://github.com/mike-engel/jwt-cli)
+brew install mike-engel/jwt-cli/jwt-cli
+brew install scrcpy
+
 # Install Homebrew GUI apps
 brew install --cask firefox docker visual-studio-code cursor raycast spotify tableplus postman ghostty proxyman git-credential-manager mockoon obsidian
 
