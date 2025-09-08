f7d3447d246a3d36d8f5c77a89f023b9dd6161d6
Knut Kirkhorn <knutkirk@hotmail.com>
2025-09-08 14:53:09 +0200

    Tweak homebrew install commands

--- macos.sh
+++ macos.sh
@@ -6,10 +6,25 @@
 # Install Homebrew packages
 # jwt-cli (https://github.com/mike-engel/jwt-cli)
 brew install mike-engel/jwt-cli/jwt-cli
-brew install scrcpy gitleaks
+brew install \
+	scrcpy \
+	gitleaks
 
 # Install Homebrew GUI apps
-brew install --cask firefox docker visual-studio-code cursor raycast spotify tableplus postman ghostty proxyman git-credential-manager mockoon obsidian
+brew install --cask \
+	firefox \
+	docker \
+	visual-studio-code \
+	cursor \
+	raycast \
+	spotify \
+	tableplus \
+	postman \
+	ghostty \
+	proxyman \
+	git-credential-manager \
+	mockoon \
+	obsidian
 
 # TODO: install node.js
 
