b09e0244ad7fead402e110b2dbb568995f53a2fe
Knut Kirkhorn <knutkirk@hotmail.com>
2026-02-13 14:29:02 +0100

    Use symlinks instead of copying files

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,15 @@
 
 git pull
 
-# Copy files to ~
-cp .aliases ~/.aliases
-cp .functions ~/.functions
-cp .bash_profile ~/.bash_profile
-cp global.gitignore ~/.gitignore
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Symlink files to ~
+ln -sf "$DOTFILES_DIR/.aliases" "$HOME/.aliases"
+ln -sf "$DOTFILES_DIR/.functions" "$HOME/.functions"
+ln -sf "$DOTFILES_DIR/.bash_profile" "$HOME/.bash_profile"
+ln -sf "$DOTFILES_DIR/global.gitignore" "$HOME/.gitignore"
 # Used to hide the login message
-cp .hushlogin ~/.hushlogin
+ln -sf "$DOTFILES_DIR/.hushlogin" "$HOME/.hushlogin"
 
 # Set global gitignore
 git config --global core.excludesfile ~/.gitignore
