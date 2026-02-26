31cb128fa10947a68edd20eb43d75e1bd2baf674
Knut Kirkhorn <knutkirk@hotmail.com>
2026-02-26 15:00:45 +0100

    Add setup script for Windows

---
+++ windows.ps1
@@ -0,0 +1,6 @@
+# GUI Apps
+# UniGetUI (https://github.com/marticliment/UniGetUI)
+winget install --exact --id MartiCliment.UniGetUI --source winget
+# GitHub Desktop (https://desktop.github.com/)
+winget install github-desktop
+
