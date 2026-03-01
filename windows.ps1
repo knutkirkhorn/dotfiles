b449c088268f65c6c3defa180ad00bd7b09457c2
Knut Kirkhorn <knutkirk@hotmail.com>
2026-03-01 14:46:52 +0100

    Add Cursor and TablePlus to Windows script

--- windows.ps1
+++ windows.ps1
@@ -3,6 +3,10 @@
 winget install --exact --id MartiCliment.UniGetUI --source winget
 # GitHub Desktop (https://desktop.github.com/)
 winget install github-desktop
+# Cursor (https://cursor.com)
+winget install --id "Anysphere.Cursor" --exact --source winget --accept-source-agreements --disable-interactivity --silent --include-unknown --accept-package-agreements --force
+# TablePlus (https://tableplus.com/)
+winget install --id "TablePlus.TablePlus" --exact --source winget --accept-source-agreements --disable-interactivity --silent --include-unknown --accept-package-agreements --force
 
 # CLI Apps
 winget install --id=astral-sh.uv  -e
