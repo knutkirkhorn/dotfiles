db11b97a80551f8099ee794e19b2fc566247f79c
Knut Kirkhorn <knutkirk@hotmail.com>
2026-05-12 16:36:47 +0200

    Move mas apps to brewfile

--- brew.Brewfile
+++ brew.Brewfile
@@ -5,3 +5,8 @@ brew "fastlane"
 
 # Mac App Store apps
 mas "Pure Paste", id: 1611378436
+mas "Week Number", id: 6502579523
+mas "Camera Preview", id: 1632827132
+# For remote desktop
+mas "Windows App", id: 1295203466
+mas "One Thing", id: 1604176982
--- macos.sh
+++ macos.sh
@@ -48,13 +48,6 @@ curl -fsSL https://bun.sh/install | bash
 # Cleanup
 brew cleanup
 
-# Install macOS apps from App Store
-mas install 1611378436 # Pure Paste
-mas install 6502579523 # Week Number
-mas install 1632827132 # Camera Preview
-mas install 1295203466 # Windows App (for remote desktop)
-mas install 1604176982 # One Thing
-
 # macOS preferences
 defaults write com.apple.finder ShowPathbar -bool true
 
